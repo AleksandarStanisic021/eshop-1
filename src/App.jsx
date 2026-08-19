5db182be8ea1543f4b303bf00842bc0a1aceeb8c
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-19 21:23:36 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <nav className="navbar">
+        <div className="nav-brand">
+          <h2>My App</h2>
+        </div>
+        <div className="nav-links">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </nav>
       <p>Hello, Vite + React! </p>
     </div>
   );
