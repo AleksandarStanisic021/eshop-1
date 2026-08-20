e47797c43962182b2a4dc961a1863e3528a11c27
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-20 13:11:44 +0200

    working on form

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,7 +1,38 @@
 export default function Auth() {
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className="page">
+      <div className="container">
+        <div className="auth-container">
+          <h1 className="page-title">Auth</h1>
+          <form className="auth-form">
+            <div className="form-group">
+              <label className="form-label" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="form-input"
+                type="email"
+                id="email"
+                name="email"
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="form-input"
+                type="password"
+                id="password"
+                name="password"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-large">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
