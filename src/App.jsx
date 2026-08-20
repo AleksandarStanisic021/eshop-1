4afe6f065505bb97bdbafbbdd9c99ae6466c0ed7
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-20 09:50:09 +0200

    startin routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
 
 function App() {
   return (
     <div className="app">
       <p>Hello, Vite + React! </p>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/auth" element={<Auth />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+      </Routes>
     </div>
   );
 }
