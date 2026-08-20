9221e167f66ca65fb12c173af0921ed69271f65d
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-20 09:35:26 +0200

    feat: add react-hook-form and react-router-dom; refactor App component to use BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <nav className="navbar">
-        <div className="nav-brand">
-          <h2>My App</h2>
-        </div>
-        <div className="nav-links">
-          <div className="hero">
-            <h1>Welcome to My App</h1>
-          </div>
-
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
-        </div>
-      </nav>
+    <div className="app">
       <p>Hello, Vite + React! </p>
     </div>
   );
