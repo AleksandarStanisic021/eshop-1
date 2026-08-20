b550968ef7bb36d139ec33f49b23e5b8d99aebed
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-20 10:38:50 +0200

    work on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,29 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-brand">
-          ShopMe
+          Shop
+          <span
+            style={{ color: "white", background: "black", padding: "0.3rem" }}>
+            Me
+          </span>
         </Link>
         <div className="navbar-links">
-          <Link to="/">Home</Link>
-          <Link to="/checkout">Cart</Link>
+          <Link className="navbar-link" to="/">
+            Home
+          </Link>
+          <Link className="navbar-link" to="/checkout">
+            Cart
+          </Link>
+        </div>
+        <div className="navbar-auth">
+          <div className="navbar-auth-links">
+            <Link to="/auth" className="btn btn-primary">
+              Login
+            </Link>
+            <Link to="/auth" className="btn btn-secondary">
+              Signup
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
