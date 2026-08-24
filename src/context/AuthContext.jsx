759f731aa5611d3dc8296f5dded13a15febc74ca
Aleksandar Stanisic <ciandre23@gmail.com>
2026-08-24 14:40:53 +0200

    add users

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,9 +3,14 @@ import { createContext, useState } from "react";
 const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
-  const [user] = useState();
+  const [user, setUser] = useState(null);
 
-  function signUp() {}
+  function signUp(email, password) {
+    const users = [];
+
+    const newUser = { email, password };
+    users.push(newUser);
+  }
 
   function Login() {}
 
